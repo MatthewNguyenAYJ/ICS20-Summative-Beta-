dae309517b8e20bf5892543d313bdcc1ecf228d2
Benjaminphan04 <bphan1@ocdsb.ca>
2018-12-06 13:35:24 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var backgroundImg;
 
   function setup(){
-  backgroundImg = loadImage("244.jpg");
+  backgroundImg = loadImage("1391869903220.png");
   createCanvas(1280,640)
 
 }
@@ -16,12 +16,12 @@ function draw() {
   fill(25,29,35);
   stroke(1,25,29,45);
   strokeWeight(12);
-  ellipse(640,320,350,350);
+  ellipse(640,280,350,350);
   
   //Fake Square (for clicking)
   fill(25,29,35);
   noStroke();
-  rect(515,195,250,250);
+  rect(515,155,250,250);
   
   // text("BlackHole",270,235,200,480);
   fill(255,255,245);

f49d6f339f0a1b88a24e55946da93ab736041aad
Benjaminphan04 <bphan1@ocdsb.ca>
2018-12-07 15:00:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,26 +6,40 @@ var backgroundImg;
 
 }
  
-function draw() {
-  
-  //background
-  
-  image(backgroundImg,10,10,1280,640);  
-  
+var blackHoleRadius = 350;
+
+var blackHole = function(){
   //blackHole Interactive
   fill(25,29,35);
   stroke(1,25,29,45);
   strokeWeight(12);
-  ellipse(640,280,350,350);
-  
-  //Fake Square (for clicking)
+  ellipse(640,280,blackHoleRadius,blackHoleRadius);
+}
+
+var fakeSquare = function() {
+  //fake Square (for clicking)
   fill(25,29,35);
   noStroke();
   rect(515,155,250,250);
-  
+}
+
+var blackholeText = function(){
   // text("BlackHole",270,235,200,480);
   fill(255,255,245);
   textSize(45);
   textFont();
+}
 
+function draw() {
+  
+  image(backgroundImg,10,10,1280,640); 
+  
+  fakeSquare();
+  blackHole();
+  blackholeText();
+  
+  if(mouseIsPressed) {
+    
+    blackHoleRadius +=300;
+  }
 }
